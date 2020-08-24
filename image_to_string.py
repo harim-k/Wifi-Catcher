940089a4a19811bd8d8e8dc7ab5e77ba7fb098c9
harim <harim9504@gmail.com>
2020-08-24 21:33:52 +0900

    modified

--- image_to_string.py
+++ image_to_string.py
@@ -1,10 +1,29 @@
-import pytesseract
+from pytesseract import *
 from PIL import Image
+import cv2
 
 
-def image_to_string(file_name):
-    text = pytesseract.image_to_string(Image.open(f"{file_name}.jpg"))
-    #print(pytesseract.image_to_string(Image.open('1.jpg'), lang='eng'))
+def convert_color_black_and_white(img, file_name):
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(f"{file_name}_BNW.jpg", img2)
+    return img2
+
+def make_image_to_string_BNW(file_name):
+
+    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
+    
+    img2 = convert_color_black_and_white(img, file_name)
+
+    text = image_to_string(img2, lang='eng')
+
+    with open(f"{file_name}_BNW.txt", "w") as text_file:
+        text_file.write(text)
+
+
+def make_image_to_string(file_name):
+    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
+
+    text = image_to_string(img, lang='eng')
 
     with open(f"{file_name}.txt", "w") as text_file:
         text_file.write(text)
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from image_to_string import image_to_string
+from image_to_string import make_image_to_string_BNW, make_image_to_string
 
 
 # for index in range(1,5):
 #     file_name = index
 #     image_to_string(file_name)
 
-file_name = index
-image_to_string(file_name)
+# for index in range(1,10):
+#     file_name = index
+#     make_image_to_string(file_name)
+#     make_image_to_string_BNW(file_name)
+
+
+file_name = input('input file name: ')
+make_image_to_string(file_name)
+make_image_to_string_BNW(file_name)
