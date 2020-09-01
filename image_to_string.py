3e641c6c8f261242ce0ac0a25b1ecc081993f6cb
harim <harim9504@gmail.com>
2020-09-01 20:35:36 +0900

    second

--- image_to_string.py
+++ image_to_string.py
@@ -1,6 +1,7 @@
 from pytesseract import *
 from PIL import Image
 import cv2
+import re
 
 
 def convert_color_black_and_white(img, file_name):
@@ -8,7 +9,19 @@ def convert_color_black_and_white(img, file_name):
     cv2.imwrite(f"{file_name}_BNW.jpg", img2)
     return img2
 
-def make_image_to_string_BNW(file_name):
+
+def convert_image_to_string_BNW(file_name):
+
+    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
+    
+    img2 = convert_color_black_and_white(img, file_name)
+
+    text = image_to_string(img2, lang='eng')
+
+    return text
+
+
+def convert_image_to_string_file_BNW(file_name):
 
     img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
     
@@ -20,10 +33,29 @@ def make_image_to_string_BNW(file_name):
         text_file.write(text)
 
 
-def make_image_to_string(file_name):
+def convert_image_to_string(file_name):
+    img = cv2.imread(f"cut/{file_name}.jpg", cv2.IMREAD_COLOR)
+
+    text = image_to_string(img, lang='eng')
+
+    return text
+
+def convert_image_to_string_file(file_name):
     img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
 
     text = image_to_string(img, lang='eng')
 
     with open(f"{file_name}.txt", "w") as text_file:
         text_file.write(text)
+
+def get_password(text):
+    password = []
+    splited_texts = re.split(' |\n', text)
+    #print(splited_texts)
+    for splited_text in splited_texts:
+        if len(splited_text) >= 8 and splited_text.isalnum() is True:
+            password.append(splited_text)
+
+    return password
+
+#print(get_password("dddddddddd ddd "))
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from image_to_string import make_image_to_string_BNW, make_image_to_string
+from image_to_string import *
 
 
 # for index in range(1,5):
 #     file_name = index
 #     image_to_string(file_name)
 
-# for index in range(1,10):
-#     file_name = index
-#     make_image_to_string(file_name)
-#     make_image_to_string_BNW(file_name)
+for index in range(1,18):
+    file_name = index
+    #convert_image_to_string_file(file_name)
 
+    text = convert_image_to_string(file_name)
+    print(f'================{index}================')
+    print('text')
+    print(text)
+    print('password')
+    password = get_password(text)
+    print(password)
+    print('====================================')
+    print()
 
-file_name = input('input file name: ')
-make_image_to_string(file_name)
-make_image_to_string_BNW(file_name)
+# file_name = input('input file name: ')
+# make_image_to_string(file_name)
+# make_image_to_string_BNW(file_name)
+
