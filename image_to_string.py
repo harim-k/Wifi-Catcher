406ba8497e092ddf2c8e8f5f26b379373fe2a79d
harim <harim9504@gmail.com>
2020-09-02 20:57:47 +0900

    third

--- image_to_string.py
+++ image_to_string.py
@@ -5,57 +5,46 @@ import re
 
 
 def convert_color_black_and_white(img, file_name):
-    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"{file_name}_BNW.jpg", img2)
-    return img2
+    BNW_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(f"{file_name}_BNW.jpg", BNW_img)
+    return BNW_img
 
 
 def convert_image_to_string_BNW(file_name):
-
     img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
-    
-    img2 = convert_color_black_and_white(img, file_name)
-
-    text = image_to_string(img2, lang='eng')
+    BNW_img = convert_color_black_and_white(img, file_name)
+    text = image_to_string(BNW_img, lang='eng')
 
     return text
-
-
-def convert_image_to_string_file_BNW(file_name):
-
-    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
-    
-    img2 = convert_color_black_and_white(img, file_name)
-
-    text = image_to_string(img2, lang='eng')
-
-    with open(f"{file_name}_BNW.txt", "w") as text_file:
-        text_file.write(text)
 
 
 def convert_image_to_string(file_name):
-    img = cv2.imread(f"cut/{file_name}.jpg", cv2.IMREAD_COLOR)
-
+    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
     text = image_to_string(img, lang='eng')
 
     return text
 
-def convert_image_to_string_file(file_name):
-    img = cv2.imread(f"{file_name}.jpg", cv2.IMREAD_COLOR)
 
-    text = image_to_string(img, lang='eng')
-
+def save_text_file(file_name, text):
     with open(f"{file_name}.txt", "w") as text_file:
         text_file.write(text)
+
 
 def get_password(text):
     password = []
     splited_texts = re.split(' |\n', text)
-    #print(splited_texts)
     for splited_text in splited_texts:
         if len(splited_text) >= 8 and splited_text.isalnum() is True:
             password.append(splited_text)
 
     return password
 
+
+def is_answer(file_name, text):
+    with open(f"answer/{file_name}.txt", "r") as text_file:
+        answer_text = text_file.readline()
+    if(answer_text == text):
+        return True
+    else:
+        return False
 #print(get_password("dddddddddd ddd "))
--- main.py
+++ main.py
@@ -5,17 +5,36 @@ from image_to_string import *
 #     file_name = index
 #     image_to_string(file_name)
 
+correct_text_ount = 0
+wrong_text_count = 0
 for index in range(1,18):
     file_name = index
-    #convert_image_to_string_file(file_name)
 
-    text = convert_image_to_string(file_name)
+    # convert image to text
+    # text = convert_image_to_string(file_name)
+    # bnw_text = convert_image_to_string(f'bnw/{file_name}_BNW')
+    # cut_text = convert_image_to_string(f'cut/{file_name}')
+    cut_bnw_text = convert_image_to_string_BNW(f'cut/{file_name}')
+    
+    text = cut_bnw_text
+    # print
     print(f'================{index}================')
-    print('text')
-    print(text)
-    print('password')
+    print(f'text:{text}')
+
     password = get_password(text)
-    print(password)
+    if(len(password) == 0):
+        password = ''
+    else:
+        password = password[0]
+
+    print(f'password: {password}')
+
+    if(is_answer(file_name, password) == True):
+        correct_text_ount += 1
+    else:
+        wrong_text_count += 1
+    
+    
     print('====================================')
     print()
 
@@ -23,3 +42,5 @@ for index in range(1,18):
 # make_image_to_string(file_name)
 # make_image_to_string_BNW(file_name)
 
+print(f'correct_text: {correct_text_ount}')
+print(f'wrong_text: {wrong_text_count}')
