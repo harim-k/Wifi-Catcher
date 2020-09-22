fc9bfedb79bf67eb3e87552fadd59836c35c6d21
harimkim <47478442+harim-k@users.noreply.github.com>
2020-09-22 22:38:11 +0900

    key_json in python

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# from image_to_string import *
 from google_vision_api import *
+import os
 
-# for index in range(1,5):
-#     file_name = index
-#     image_to_string(file_name)
+# depend on your path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/harimkim/Desktop/key.json"
+
 
 correct_text_ount = 0
 wrong_text_count = 0
@@ -17,36 +17,3 @@ for index in range(1,18):
     print(text_list)
     password_list = get_password(text_list)
     print(f'password:{password_list}')
-
-    # bnw_text = convert_image_to_string(f'bnw/{file_name}_BNW')
-    # cut_text = convert_image_to_string(f'cut/{file_name}')
-    #cut_bnw_text = convert_image_to_string_BNW(f'cut/{file_name}')
-    
-    # text = bnw_text
-    # print
-    
-    
-
-#     password = get_password(text)
-#     if(len(password) == 0):
-#         password = ''
-#     else:
-#         password = password[0]
-
-#     print(f'predicted password: {password}')
-
-#     if(is_answer(file_name, password) == True):
-#         correct_text_ount += 1
-#     else:
-#         wrong_text_count += 1
-    
-    
-#     print('====================================')
-#     print()
-
-# # file_name = input('input file name: ')
-# # make_image_to_string(file_name)
-# # make_image_to_string_BNW(file_name)
-
-# print(f'correct_text: {correct_text_ount}')
-# print(f'wrong_text: {wrong_text_count}')
